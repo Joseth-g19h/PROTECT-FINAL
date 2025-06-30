356fb86622ea098cc1d8e37249ff6715d8c832cb
Joseth-g19h <garciahernandezjoseph9@gmail.com>
2025-06-30 15:16:04 -0600

    Primer commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors()); // Permitir conexión con el frontend
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hola desde el backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend en http://localhost:${PORT}`);
+});
